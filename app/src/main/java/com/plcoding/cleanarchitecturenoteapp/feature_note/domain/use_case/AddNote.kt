3e9ba649f2da88ec62541d3d2007f730e5ebba9e
ilhuitem0c <jc11111001001@gmail.com>
2022-08-01 16:26:29 -0500

    Use Cases

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/AddNote.kt
@@ -0,0 +1,20 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.InvalidNoteException
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class AddNote(
+    private val repository: NoteRepository
+) {
+    @Throws(InvalidNoteException::class)
+    suspend operator fun invoke(note: Note){
+        if(note.title.isBlank()){
+            throw InvalidNoteException("El titulo de la nota no puede estar vacio")
+        }
+        if (note.content.isBlank()){
+            throw InvalidNoteException("El contenido de la nota no puede estar vacio")
+        }
+        repository.insertNote(note)
+    }
+}
